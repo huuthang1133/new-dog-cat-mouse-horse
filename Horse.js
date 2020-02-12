1b38faa430c2e295ed6ae79d1fb827fa96e7143e
CodersTokyo <info@coders.tokyo>
2020-02-12 10:44:56 +0700

    Add Horse Class

--- Horse.js
+++ Horse.js
@@ -1,12 +1,5 @@
-function Horse() {
+function Horse  (){
+
 }
-
-Horse.prototype.jump = function() {
-  console.log('Jumped...');
-};
-
-Horse.prototype.run = function() {
-	console.log('Running');
-};
 
 module.exports = Horse;
